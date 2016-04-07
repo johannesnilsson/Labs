453a31419e1572e31d162f5d9085f633f8475e59
emirhusic <emir.husic@gmx.com>
2016-04-07 11:01:54 +0200

    okej nu commitar jag bror.

--- Labs/src/Lab1/womAn.java
+++ Labs/src/Lab1/womAn.java
@@ -3,7 +3,7 @@ package Lab1;
 public class womAn {
 	public static void main(String [] args)
 	{
-		System.out.println(" hejsan hopsan");
+		System.out.println(" hejsadawdwaawwadn hopsan");
 	}
 
 }
