e3af6cdd2a22bf35b92b41db8407d79233aab169
johannesnilsson <Johannesnilsson2@gmail.com>
2016-04-26 16:19:09 +0200

    Additional cleanup!

--- Labs/src/myTestobjects/EmkicTest.java
+++ Labs/src/myTestobjects/EmkicTest.java
@@ -6,37 +6,9 @@ import java.util.ArrayList;
 
 public class EmkicTest {
 		public static void main(String[]args){
-			//id,x,y,
 
-		Points p1 = new Points(1,13,10);
-		Points p2 = new Points(2,4,32);
-		Points p3 = new Points(3,1,1);
-		Points p4 = new Points(4,200,2);
-		Points p5 = new Points(5,300,300);
-		Points p6 = new Points(6,301,301);
-		Points p7 = new Points(7,377,377);
-		Points p8 = new Points(8,377.5,377);
-		
 		ArrayList<Points> temp = new ArrayList<Points>();
-		//temp.add(p1);
-		temp.add(p2);
-		temp.add(p3);
-		temp.add(p4);
-		temp.add(p5);
-		temp.add(p6);
-		temp.add(p7);
-		temp.add(p8);
-		temp.add(p1);
-//		for(int i = 0; i < temp.size(); i++){
-//			System.out.println(temp.get(i).getID());
-//		}
-		//public ClosestPair(ArrayList<Points> points){
-//		Parser myParser = new Parser();
-//		temp =myParser.doParse("ulysses16");
-//		
-//		
-//		ClosestPair test = new ClosestPair(temp, "ulysses16.tsp", "1084");
-		
+			
 		Parser myParser = new Parser();
         @SuppressWarnings("unused")
         ClosestPair test = null;
